87c0e34fccfb5f5e225f46c87cd68f21f472b6bc
Embarrass9426 <pubgsmhm@gmail.com>
2026-06-10 18:35:10 +0800

    use dockerfile for railway deployment

--- backend/build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# Render build script - runs during deployment
-# Install system dependencies for pyzbar
-apt-get update && apt-get install -y libzbar0
-
-# Install Python dependencies
-pip install -r requirements.txt
